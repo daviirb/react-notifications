8ba567647ae24843328019b59f8877802a1e39e6
Davi Ribeiro <davi.rb@hotmail.com>
2023-12-19 01:04:34 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Widget } from "@/components/Widget";
+
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-black flex items-center justify-center text-zinc-50">
+      <Widget />
+    </main>
+  )
+}
