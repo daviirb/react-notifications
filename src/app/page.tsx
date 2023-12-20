dd6e31a3bb50d34ceacf90110f0d00bc59f5d988
Davi Ribeiro <davi.rb@hotmail.com>
2023-12-19 22:33:33 -0300

    dark/light mode theme switch added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Widget } from "@/components/Widget";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-black flex items-center justify-center text-zinc-50">
+    <main className="h-screen bg-zinc-50 dark:bg-black flex items-center justify-center text-zinc-900 dark: text-zinc-50">
       <Widget />
     </main>
   )
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,64 +4,64 @@ import { Rocket, X, Check } from 'lucide-react'
 export function Widget() {
     return (
         <div className="w-[448px] rounded overflow-hidden">
-            <div className="bg-zinc-900 py-4 px-6 flex items-center justify-between">
+            <div className="bg-zinc-200 dark:bg-zinc-900 py-4 px-6 flex items-center justify-between">
                 <span className="font-bold">Notification</span>
                 <button className="text-violet-500 font-bold text-xs hover:text-violet-400">MARCAR TODAS COMO VISTAS</button>
             </div>
 
             <div>
-                <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc dark:text-zinc-500 dark:text-zinc-400">
                     Recentes
                 </div>
-                <div className='divide-y-2 divide-zinc-950'>
-                <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className='divide-y-2 divide-zinc-300 dark:divide-zinc-950'>
+                <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                     <Rocket className='w-6 h-6 text-violet-500 mt-3'/>
                     <div className='flex-1 flex flex-col gap-2'>
-                        <p className='text-sm leading-relaxed text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
-                        <div className='text-xxs text-zinc-400 flex items-center gap-1'>
+                        <p className='text-sm leading-relaxed text-zinc-700 dark:text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
+                        <div className='text-xxs text-zinc-700 dark:text-zinc-400 flex items-center gap-1'>
                             <span>Convite</span>
                             <span>Há 3 min</span>
                         </div>
                     </div>
                 </div>
-                <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                     <Rocket className='w-6 h-6 text-violet-500 mt-3'/>
                     <div className='flex-1 flex flex-col gap-2'>
-                        <p className='text-sm leading-relaxed text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
-                        <div className='text-xxs text-zinc-400 flex items-center gap-1'>
+                        <p className='text-sm leading-relaxed text-zinc-700 dark:text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
+                        <div className='text-xxs text-zinc-700 dark:text-zinc-400 flex items-center gap-1'>
                             <span>Convite</span>
                             <span>Há 3 min</span>
                         </div>
                     </div>
                     <div className='flex gap-2 self-center'>
-                        <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-800  hover:bg-zinc-700'>
+                        <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-500 dark:bg-zinc-800  hover:bg-zinc-600'>
                             <X className='w-4 h-4 text-zinc-50' />
                         </button>
-                        <button className='w-8 h-8 rounded flex items-center justify-center  bg-violet-500 rounded hover:bg-violet-600'>
-                            <Check className='w-4 h-4'/>
+                        <button className='w-8 h-8 rounded flex items-center justify-center  bg-violet-600 rounded hover:bg-violet-700'>
+                            <Check className='w-4 h-4 text-zinc-50'/>
                         </button>
                     </div>
                 </div>
                 </div>
-                <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+                <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm bg-zinc-800 text-zinc-600 dark:text-zinc-400">
                     Antigas
                 </div>
-                <div className='divide-y-2 divide-zinc-950'>
-                <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className='divide-y-2 divide-zinc-300 dark:divide-zinc-950'>
+                <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                     <Rocket className='w-6 h-6 text-violet-500 mt-3'/>
                     <div className='flex-1 flex flex-col gap-2'>
-                        <p className='text-sm leading-relaxed text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
-                        <div className='text-xxs text-zinc-400 flex items-center gap-1'>
+                        <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
+                        <div className='text-xxs text-zinc-600 dark:text-zinc-400 flex items-center gap-1'>
                             <span>Convite</span>
                             <span>Há 3 min</span>
                         </div>
                     </div>
                 </div>
-                <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                     <Rocket className='w-6 h-6 text-violet-500 mt-3'/>
                     <div className='flex-1 flex flex-col gap-2'>
-                        <p className='text-sm leading-relaxed text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
-                        <div className='text-xxs text-zinc-400 flex items-center gap-1'>
+                        <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat</p>
+                        <div className='text-xxs text-zinc-600 dark:text-zinc-400 flex items-center gap-1'>
                             <span>Convite</span>
                             <span>Há 3 min</span>
                         </div>
